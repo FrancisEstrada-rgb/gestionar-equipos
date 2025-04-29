f669b413a4ead8944b5f5b9bc5eb32107fac1531
FrancisEstrada-rgb <efranciselizabeth@gmail.com>
2025-04-28 19:28:41 -0600

    Update EquipoFactory.php

--- database/factories/EquipoFactory.php
+++ database/factories/EquipoFactory.php
@@ -13,7 +13,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class EquipoFactory extends Factory
 {
     /**
-     * Define the model's default state.
+     * 
      *
      * @return array<string, mixed>
      */
